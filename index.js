27e66fa544a7003a16103287099478e1a1f00cd5
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-27 18:19:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ return name.map(function(driver){
 });
 }
 
-function nameToAttributes(name){
-  return name.map(function(driver){
+function nameToAttributes(drivers){
+  return drivers.map(function(driver){
     return {firstName: driver.split('')[0], lastName: driver.split('')[1]};
   });
 }
